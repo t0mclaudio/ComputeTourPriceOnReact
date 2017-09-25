ebc2dcdf8fc3b96feef968c6f788688c76f4eea9
Tom Claudio <t0mclaudio@Toms-Imac.local>
2017-09-25 12:09:01 +0800

    Create a dummy data file

---
+++ src/components/data.js
@@ -0,0 +1,43 @@
+var data = {
+	"name": "Tom Claudio",
+	"date": "2017-09-22",
+	"contactno": "09177011882",
+	"email": "tom@etours.ph",
+	"adults": 5,
+	"k02": 0,
+	"k35": 0,
+	"k61": 0,
+	"individual": [{
+			"item": "Pearl Farm",
+			"price": 3000
+		},
+		{
+			"item": "Crocodile Park",
+			"price": 200
+		},
+		{
+			"item": "PEC",
+			"price": 100
+		},
+		{
+			"item": "Eden",
+			"price": 650
+		}
+	],
+	"shared": [{
+			"item": "Van",
+			"price": 8000
+		},
+		{
+			"item": "Tour Guide",
+			"price": 1800
+		}
+	],
+	"i_total": 0,
+	"s_total": 0,
+	"margin": 0,
+	"tax": 0,
+	"total": 0
+}
+
+export default data;
