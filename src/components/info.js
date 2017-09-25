0685236fbc9a7c6f266eb981cd991ef09a2ed382
Tom Claudio <t0mclaudio@Toms-Imac.local>
2017-09-25 15:19:30 +0800

    Cleanup label for to htmlFor JSX compliant

--- src/components/info.js
+++ src/components/info.js
@@ -24,39 +24,39 @@ var Info = React.createClass({
           <h1>Customer Information</h1>
           <div className="row row1">
             <div id="nameGroup" className="input-group">
-              <label for="name" className="label">Name</label>
+              <label htmlFor="name" className="label">Name</label>
               <input type="text" name="name" className="input" value={this.state.name} onChange={this.onChange} />
             </div>
             <div id="dateGroup" className="input-group">
-              <label for="date" className="label">Date</label>
+              <label htmlFor="date" className="label">Date</label>
               <input type="date" name="date" className="input" value={this.state.date} onChange={this.onChange}  />
             </div>  
           </div>
           <div className="row pax">
             <div className="input-group">
-              <label for="adults" className="label">Adults</label>
+              <label htmlFor="adults" className="label">Adults</label>
               <input type="number" name="adults" className="input numPax"  value={this.state.adults} onChange={this.onChange} />
             </div>
             <div className="input-group">  
-              <label for="k02" className="label">Kid (0-2 yrs)</label>
+              <label htmlFor="k02" className="label">Kid (0-2 yrs)</label>
               <input type="number" name="k02" className="input numPax" value={this.state.k02} onChange={this.onChange}  />
             </div>
             <div className="input-group">  
-              <label for="k35" className="label">Kid (3-5 yrs)</label>
+              <label htmlFor="k35" className="label">Kid (3-5 yrs)</label>
               <input type="number" name="k35" className="input numPax" value={this.state.k35} onChange={this.onChange}  />
             </div>
             <div className="input-group">  
-              <label for="k61" className="label">Kid (6-11 yrs)</label>
+              <label htmlFor="k61" className="label">Kid (6-11 yrs)</label>
               <input type="number" name="k61" className="input numPax" value={this.state.k61} onChange={this.onChange}  />
             </div>  
           </div>
           <div className="row">
             <div className="input-group">
-              <label for="contactno" className="label">Contact number</label>
+              <label htmlFor="contactno" className="label">Contact number</label>
               <input type="text" name="contactno" className="input" value={this.state.contactno} onChange={this.onChange}  />
             </div>
             <div className="input-group">
-              <label for="email" className="label">Email Address</label>
+              <label htmlFor="email" className="label">Email Address</label>
               <input type="text" name="email" className="input" value={this.state.email} onChange={this.onChange}  />
             </div>  
           </div>
