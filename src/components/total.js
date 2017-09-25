2783d1884b17b0afa8679cf3064cf06b80e62fe5
Tom Claudio <t0mclaudio@Toms-Imac.local>
2017-09-25 15:19:44 +0800

    Create total component

---
+++ src/components/total.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+var Total = props => {
+    return (
+        <div className="">
+            <h2>Total: {props.total}</h2>
+        </div>    
+    )
+}
+
+export default Total;
