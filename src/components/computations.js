866726b38e46d14813fc5018ee552738b31b9ac7
Tom Claudio <t0mclaudio@Toms-Imac.local>
2017-09-25 12:12:47 +0800

    Remove data and convert to arrow function

--- src/components/computations.js
+++ src/components/computations.js
@@ -1,27 +1,24 @@
 import React from 'react';
 import CostTable from './costtable.js';
 
-var I_ITEMS = [
-  {"item": "Pearl Farm", "price": 3000},
-  {"item": "Crocodile Park", "price": 200},
-  {"item": "PEC", "price": 100},
-  {"item": "Eden", "price": 650}
-]
-
-var S_ITEMS = [
-  {"item": "Van", "price": 8000},
-  {"item": "Tour Guide", "price": 1800}
-]
-
-var Computations = React.createClass({
-    render() {
+var Computations = (props) => {
+        const onAdd = obj => props.addNewItem(obj)    
         return (
             <div className="container3">  
-                <CostTable title="Individual" icon="fa-user" items={I_ITEMS}/> 
-                <CostTable title="Shared" icon="fa-users" items={S_ITEMS}/> 
+                <CostTable 
+                    title="Individual" 
+                    total={props.data.i_total} 
+                    icon="fa-user" 
+                    items={props.data.individual}
+                    onAddItem = {(obj)=>onAdd(obj)}/> 
+
+                <CostTable 
+                    title="Shared" 
+                    total={props.data.s_total} 
+                    icon="fa-users" 
+                    items={props.data.shared}/> 
           </div>
         )
     }
-})
 
 export default Computations;
