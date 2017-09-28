c83784d20de9117e8c9e14841e3c69bc9e442cdc
Tom Claudio <t0mclaudio@Toms-Imac.local>
2017-09-28 12:30:17 +0800

    Convert to fx component

--- src/components/additem.js
+++ src/components/additem.js
@@ -1,46 +1,23 @@
 import React from 'react';
 
-class AddItem extends React.Component {
-  constructor(props){
-    super(props);
-    this.onItemChange = this.onItemChange.bind(this);
-    this.onPriceChange = this.onPriceChange.bind(this);
-    this.onSubmit = this.onSubmit.bind(this);
-    this.state = {item: "", price:0};
+const AddItem = props => {
+  const onAddItem = event =>{
+    event.preventDefault();
+    props.onAddItem(event)
   }
-  onItemChange(e){
-    this.setState({
-      item: e.target.value,
-    })
-  }
-  onPriceChange(e){
-    this.setState({
-      price: Number(e.target.value)
-    })
-  }
-  onSubmit(e){
-    e.preventDefault()
-    this.props.onAddItem(this.state)
-    this.setState({
-      item: "",
-      price: 0
-    })
-  }
-  render(){
-    return (
-      <tr>
-        <td colSpan={2}>
-          <div className="footer-control">
-            <form onSubmit={this.onSubmit}>
-              <input name="item" type="text" placeholder="Item" value={this.state.item} onChange={this.onItemChange} />
-              <input name="price" type="number" placeholder="Price" value={this.state.price} onChange={this.onPriceChange}  />
-              <input type="submit" value="Add" />
-            </form>
-          </div>  
-        </td>
-      </tr>    
-    )
-  }
+  return (
+    <tr>
+      <td colSpan={2}>
+        <div className="footer-control">
+          <form onSubmit={onAddItem}>
+            <input name="item" type="text" placeholder="Item" />
+            <input name="price" type="number" placeholder="Price" />
+            <input type="submit" value="Add" />
+          </form>
+        </div>  
+      </td>
+    </tr>    
+  )
 }
 
 export default AddItem;
