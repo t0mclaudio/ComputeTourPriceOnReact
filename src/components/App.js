151e6e12c3ff6bcc159b07a395262727041f50a1
Tom Claudio <t0mclaudio@Toms-Imac.local>
2017-09-28 13:49:12 +0800

    Add compnent will mount refactor computations of totals

--- src/components/App.js
+++ src/components/App.js
@@ -16,24 +16,36 @@ class App extends React.Component {
     super(props);
     this.state = data
   }
+  componentWillMount(){
+    this.computeTotals();
+  }
+  computeTotals(){
+    let individual = this.state.individual
+    let shared = this.state.shared
+    let itotal = individual.reduce((total, item)=>({price: total.price+item.price}))
+    let stotal = shared.reduce((total, item)=>({price: total.price+item.price}))    
+    this.setState(
+      { 
+        i_total:itotal.price,
+        s_total:stotal.price
+      }
+    )
+    this.setState((prevState, props)=>({
+      total: prevState.i_total + (prevState.s_total/prevState.adults)
+    }))     
+  }
   appendItem(type, obj) {
     let item = {item: obj.target.item.value, price: Number(obj.target.price.value) }
     if (type==="i") {
       let individual = this.state.individual
-      individual.push(item)
-      let total = individual.reduce((total, item)=>({price: total.price+item.price}))    
+      individual.push(item) 
       this.setState({individual: individual})
-      this.setState({i_total:total.price})
     } else if (type==="s") {
       let shared = this.state.shared
-      shared.push(item)
-      let total = shared.reduce((total, item)=>({price: total.price+item.price}))    
+      shared.push(item)  
       this.setState({shared: shared})
-      this.setState({s_total:total.price})   
     }
-    this.setState((prevState, props)=>({
-      total: prevState.i_total + (prevState.s_total/prevState.adults)
-    })) 
+    this.computeTotals();
   }
   updateInfoState(event){
     this.setState({
