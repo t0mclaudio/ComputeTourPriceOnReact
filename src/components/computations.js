49d1bb01bf8cf3a96cc2f2fd82628470cad4b382
Tom Claudio <t0mclaudio@Toms-Imac.local>
2017-09-28 16:12:40 +0800

    Create fallback fx for Factor Component

--- src/components/computations.js
+++ src/components/computations.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import CostTable from './costtable.js';
 import Total from './total.js';
+import Factor from './factor';
 
 var Computations = (props) => {
-        const onAdd = (type,obj) => props.addNewItem(type,obj)    
+        const onAdd = (type,obj) => props.addNewItem(type,obj)  
+        const onFactorChange = event => props.factorChange(event) 
         return (
-            <div className="container3">  
+            <div className="container3">
+                <Factor onChange={(event)=>onFactorChange(event)} data={props.data} />  
                 <CostTable 
                     title="Individual" 
                     total={props.data.i_total} 
-                    icon="fa-user" 
+                    icon="fa-user"
                     items={props.data.individual}
                     onAddItem = {(obj)=>onAdd("i", obj)}/> 
 
