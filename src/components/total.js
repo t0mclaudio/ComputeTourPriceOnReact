285b3f9994c5fd781b6bb5caf6372f3ca403b4e7
Tom Claudio <t0mclaudio@Toms-Imac.local>
2017-10-10 16:24:29 +0800

    Make accept text props

--- src/components/total.js
+++ src/components/total.js
@@ -4,8 +4,8 @@ import Currency from 'currency-formatter';
 var Total = props => {
     let total = Currency.format(props.total, {code: 'PHP'});
     return (
-        <div className="">
-            <h2>Total: {total}</h2>
+        <div className="row">
+            <p>{props.text}: {total}</p>
         </div>    
     )
 }
